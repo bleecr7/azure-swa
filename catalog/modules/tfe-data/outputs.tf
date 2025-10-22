2b2d4ebfbddfa516d542b97914985afa2077f5f0
bleecr7 <blee328official@gmail.com>
2025-10-22 23:27:43 +0100

    Updated workflow to pull React repo and build web app in SWA using API key
    Added az login step
    
    Added Azure creds for az login
    
    typo fix

--- catalog/modules/tfe-data/outputs.tf
+++ catalog/modules/tfe-data/outputs.tf
@@ -1,6 +1,6 @@
 output "remote_state_data" {
   value = {
-    dns_zone = data.tfe_outputs.remote_state.values.dns_info
+    dns_info = data.tfe_outputs.remote_state.values.dns_info
     key_vault_info  = data.tfe_outputs.remote_state.values.key_vault_info
     storage_account_info = data.tfe_outputs.remote_state.values.storage_account_info
     tfstate_storage_container_info = data.tfe_outputs.remote_state.values.tfstate_storage_container_info
